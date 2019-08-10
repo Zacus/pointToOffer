5d1e616ad4a6477271798115cf134ba88e46eb3d
Zacus <zhaoshuais@icloud.com>
2019-08-10 17:18:19 +0800

    暂时更新

---
+++ pointToOffer/pch.cpp
@@ -0,0 +1,19 @@
+﻿// pch.cpp: 与预编译标头对应的源文件；编译成功所必需的
+
+#include "pch.h"
+#include<math.h>
+
+double Solution::Power(double base, int exponent) {
+	double cnt=base;
+	if (base < 0.0000001)
+		return 1;
+	double result = 1;
+	while (exponent) {
+		if (exponent & 1) {
+			result *= cnt;
+		}
+		cnt*= cnt;
+		exponent >>= 1;
+	}
+	return result;
+};
---
+++ pointToOffer/pointToOffer.cpp
@@ -0,0 +1,14 @@
+﻿// pointToOffer.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	Solution s;
+	cout<<s.Power(2.0, 4)<<endl;
+
+}
+
